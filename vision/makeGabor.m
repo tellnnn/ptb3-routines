a47d3961411cde648ea5b8a658a7b61f53ee35ce
tellnnn <tellurium3262@gmail.com>
2025-06-16 14:48:03 -0400

    make res parameter into name-value parameters

--- vision/makeGabor.m
+++ vision/makeGabor.m
@@ -1,6 +1,6 @@
-function img = makeGabor(res, gratingParams, maskParams)
+function img = makeGabor(params)
 % makeGabor creates a Gabor image by combining a grating and a mask.
-%  img = makeGabor(res, [name-value pairs])
+%  img = makeGabor([name-value pairs])
 %    creates a Gabor image with specified grating and mask parameters.
 %
 %  Input:
@@ -13,8 +13,8 @@ function img = makeGabor(res, gratingParams, maskParams)
 %
 %  Example:
 %    img = makeGabor;
-%    img = makeGabor(512);
-%    img = makeGabor(512, 'ori', -45);
+%    img = makeGabor('res', 512);
+%    img = makeGabor('ori', -45);
 %    
 %    diameter = 10; % diameter of Gabor in degree 
 %    freq = 1; % spatial frequency in degree
@@ -22,23 +22,25 @@ function img = makeGabor(res, gratingParams, maskParams)
 %    img = makeGabor('freq', freq * diameter, 'sigma', sigma / diameter);
 
 arguments (Input)
-    res (1,1) double {mustBePositive} = 256 % resolution of the image (default 256)
+    params.res (1,1) double {mustBePositive} = 256 % resolution of the image (default 256)
 
-    gratingParams.ori (1,1) double = 0 % orientation in degrees (0 indicates vertical: default 0)
-    gratingParams.freq (1,1) double {mustBePositive} = 1 % frequency in cycles per degree (default 1)
-    gratingParams.phase (1,1) double = 0 % phase in degrees (default 0)
-    gratingParams.contrast (1,1) double {mustBeInRange(gratingParams.contrast, 0, 1)} = 1 % contrast (0 to 1: default 1)
+    % grating parameters
+    params.ori (1,1) double = 0 % orientation in degrees (0 indicates vertical: default 0)
+    params.freq (1,1) double {mustBePositive} = 1 % frequency in cycles per degree (default 1)
+    params.phase (1,1) double = 0 % phase in degrees (default 0)
+    params.contrast (1,1) double {mustBeInRange(params.contrast, 0, 1)} = 1 % contrast (0 to 1: default 1)
 
-    maskParams.sigma (1,1) double {mustBePositive} = 0.3 % standard deviation of the Gaussian in fraction of the image size (default 0.3)
+    % Gaussian envelop parameters
+    params.sigma (1,1) double {mustBePositive} = 0.3 % standard deviation of the Gaussian envelope in fraction of the image size (default 0.3)
 end
 
 arguments (Output)
     img (:,:) double {mustBeInRange(img, -1, 1)} % Gabor image with values between -1 and 1 in size (res, res)
 end
 
-grating = makeGrating('res', res, 'ori', gratingParams.ori, 'freq', gratingParams.freq, 'phase', gratingParams.phase, 'contrast', gratingParams.contrast);
+grating = makeGrating('res', params.res, 'ori', params.ori, 'freq', params.freq, 'phase', params.phase, 'contrast', params.contrast);
 
-gauss = makeMask('res', res, 'type', 'gauss', 'sigma', maskParams.sigma);
+gauss = makeMask('res', params.res, 'type', 'gauss', 'sigma', params.sigma);
 
 img = grating .* gauss;
 
